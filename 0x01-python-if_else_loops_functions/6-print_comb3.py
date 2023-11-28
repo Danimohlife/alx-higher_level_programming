c918015395db72041c1d40e28144f65780b8cd94
danimohlife <danimohlife@yahoo.com>
2023-11-28 14:30:42 -0800

    chr reduce

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,7 @@
 
 for num1 in range(9):
     for num2 in range(10):
-        if num1 < num2:
+        if num1 < num2 and num1 != 8:
             print("{}{}, ".format(num1, num2), end='')
-print("{}{}".format(num1, num2))
+        elif num1 < num2:
+            print("{}{}".format(num1, num2))
