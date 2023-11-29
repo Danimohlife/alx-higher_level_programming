84f96e99929f94c873c531b77cdbb92516af6f09
danimohlife <danimohlife@yahoo.com>
2023-11-29 08:43:02 -0800

    char reduced

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for x in range(1, 101):
-
-        if x % 3 == 0 and x % 3 == 0:
-            print("{} ".format("FizzBuzz"), end='')
-        elif x % 3 == 0:
-            print("{} ".format("Fizz"), end='')
-        elif x % 5 == 0:
-            print("{} ".format("Buzz"), end='')
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print("{} ".format(x), end='')
+            print("{} ".format(number), end="")
