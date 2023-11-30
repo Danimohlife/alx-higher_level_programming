45ca7799194816c00c55cd92d57608cd227cc12d
danimohlife <danimohlife@yahoo.com>
2023-11-30 11:43:28 -0800

    arjusted format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     l_st = sys.argv
     ln = len(l_st) - 1
-    print("{:d} {}".format(ln, 'argument.' if ln <= 1 else 'arguments:'))
+    print("{:d} {}".format(ln, 'argument.' if ln == 1 or ln == 0 else 'arguments:'))
 
     ln += 1
     for i in range(1, ln):
