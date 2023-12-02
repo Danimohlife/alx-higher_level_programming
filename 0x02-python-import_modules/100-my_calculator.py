46dae91224df34b5bd586824f871b77c773213bd
danimohlife <danimohlife@yahoo.com>
2023-12-02 00:34:29 -0800

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,30 +2,32 @@
 
 from calculator_1 import add, sub, mul, div
 import sys
+import argparse
+
 if __name__ == "__main__":
     ln = len(sys.argv)
-
-    if ln == 4:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
 
     if ln != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    for sign in "*+/-":
-        if sys.argv[2] == sign and sign == '*':
-            print("{:d} {} {:d} = {:d}".format(a, sign, b, a * b))
-            break
-        elif sys.argv[2] == sign and sign == '/':
-            print("{:d} {} {:d} = {:d}".format(a, sign, b, a / b))
-            break
-        elif sys.argv[2] == sign and sign == '+':
-            print("{:d} {} {:d} = {:d}".format(a, sign, b, a + b))
-            break
-        elif sys.argv[2] == sign and sign == '-':
-            print("{:d} {} {:d} = {:d}".format(a, sign, b, a * b))
-            break
+    parser = argparse.ArgumentParser(description='Commandline Math Programme')
+    parser.add_argument('firstNum', help='First Number')
+    parser.add_argument('sign', help='sign to use')
+    parser.add_argument('secondNum', help='Second Number')
+    args = parser.parse_args()
+
+    a = int(args.firstNum)
+    b = int(args.secondNum)
+
+    if args.sign == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif args.sign == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    elif args.sign == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif args.sign == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     else:
         print("{}".format("Unknown operator. Available operators: +, -, * and /"))
         sys.exit(1)
