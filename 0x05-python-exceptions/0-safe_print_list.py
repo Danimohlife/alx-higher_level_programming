761bd3124baeca574b2e8f99650ebc27c182111f
ddanimoh <ddanimoh@gmail.com>
2025-03-14 12:12:51 +0100

    ag

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,8 +14,17 @@ def safe_print_list(my_list=[], x=0):
 
     for lop in range(x):
         try:
-            print("{:}".format(my_list[lop], end=""))
+            print(my_list[lop], end='')
             count += 1
         except IndexError:
+            print()
             return count
     return count
+
+
+
+my_list = [1, 2, 3, 4]
+x = len(my_list) + 1
+
+nb_print = safe_print_list(my_list, x)
+print("{:d}".format(nb_print))
