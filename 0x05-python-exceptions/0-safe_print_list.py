eafb54b8586fd95e5544f67fac9610f4806e4644
ddanimoh <ddanimoh@gmail.com>
2025-03-14 12:14:40 +0100

    ag

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -20,11 +20,3 @@ def safe_print_list(my_list=[], x=0):
             print()
             return count
     return count
-
-
-
-my_list = [1, 2, 3, 4]
-x = len(my_list) + 1
-
-nb_print = safe_print_list(my_list, x)
-print("{:d}".format(nb_print))
