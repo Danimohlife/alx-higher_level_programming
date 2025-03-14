b65f7a6731421e8889ab0c5027f5940b99441f82
ddanimoh <ddanimoh@gmail.com>
2025-03-14 12:21:45 +0100

    rl

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,9 +15,9 @@ def safe_print_list(my_list=[], x=0):
     for lop in range(x):
         try:
             print(my_list[lop], end='')
+            count += 1
         except IndexError:
             pass
-        count += 1
         
     print()
     return count
