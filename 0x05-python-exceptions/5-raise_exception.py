44a5935bf3720a7b2333f376c6c7f8765892846f
ddanimoh <ddanimoh@gmail.com>
2025-03-15 09:18:35 +0100

    raise

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+raise a type except
+"""
+
+
+def raise_exception():
+    """
+    exception raise
+    """
+
+    try:
+        raise TypeError
+    except TypeError:
+        raise TypeError
