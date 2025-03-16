c15f0d5f8683a1cb78cb04ecaea509755253705d
ddanimoh <ddanimoh@gmail.com>
2025-03-16 22:45:33 +0100

    size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,7 +8,10 @@ of size with no verification of type
 class square:
     """
     square with size instantiation
+
+    a class doc
     """
+
     def __init__(self, size):
         """
         init private size attribute
