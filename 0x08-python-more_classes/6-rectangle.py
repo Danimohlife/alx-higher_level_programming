b651c43fbeb35a692177232ace9345476d153178
ddanimoh <ddanimoh@gmail.com>
2025-03-24 08:46:51 +0100

    publuc

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances =+ 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -176,5 +176,5 @@ class Rectangle:
 
         Example:
         """
-        Rectangle.number_of_instances =+ 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
