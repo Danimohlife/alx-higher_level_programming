e61c71496094967c596d7d94b075749889821bf8
ddanimoh <ddanimoh@gmail.com>
2025-03-24 12:53:38 +0100

    cls

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -195,6 +195,7 @@ class Rectangle:
 
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
+    @classmethod
     def square(cls, size=0):
         """
         Creates a new instance of Rectangle where width
