9f2eeab6633aa541a84d84bc73ab3cb10b3505a4
ddanimoh <ddanimoh@gmail.com>
2025-03-25 11:56:32 +0100

    geo

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module defining a base class for geometric shapes.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometric operations.
+
+    This class serves as a foundation for more
+    specific geometry-related classes.
+    Currently, it does not implement any methods or
+    attributes but can be extended
+    in future subclasses.
+
+    Example:
+        >>> bg = BaseGeometry()
+        >>> isinstance(bg, BaseGeometry)
+        True
+    """
+    pass
