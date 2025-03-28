9e0ebae0633888c8e389c47d45c61b062e85ef77
ddanimoh <ddanimoh@gmail.com>
2025-03-28 09:50:58 +0100

    item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,17 +12,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item():
-    """
-    If the file doesn’t exist, it should be created
-    You don’t need to manage file permissions /
-    exceptions.
-    """
-    filename = "add_item.json"
+"""
+If the file doesn’t exist, it should be created
+You don’t need to manage file permissions /
+exceptions.
+"""
+filename = "add_item.json"
 
-    data = load_from_json_file(filename) if os.path.exists(filename) else []
-    data.extend(sys.argv[1:])
-    save_to_json_file(data, filename)
-
-
-add_item()
+data = load_from_json_file(filename) if os.path.exists(filename) else []
+data.extend(sys.argv[1:])
+save_to_json_file(data, filename)
