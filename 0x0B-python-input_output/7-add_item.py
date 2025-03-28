901dad0cba15068c7e1532e94599c660b2dc4f88
ddanimoh <ddanimoh@gmail.com>
2025-03-28 09:38:56 +0100

    item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-
 """
-A script that adds all command-line arguments to a
-Python list and saves them to a file.
-If the file doesn’t exist, it should be created
-You don’t need to manage file permissions / exceptions
+Module with a script that adds arguments
+to a Python list saved in a file
 """
 
 import sys
-import json
+import os
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item():
-    """
-    If the file doesn’t exist, it should be created
-    You don’t need to manage file permissions /
-    exceptions.
-    """
-    filename = "add_item.json"
+file = 'add_item.json'
+if os.path.exists(file):
+    # if it exists, called it, otherwise
+    # make a new one
+    thd = load_from_json_file(file)
+else:
+    with open(file, 'w') as f:
+        thd = []
+        save_to_json_file([], file)
 
-    data = load_from_json_file(filename) if os.path.exists(filename) else []
-
-    data.extend(sys.argv[1:])
-    save_to_json_file(data, filename)
-
-
-add_item()
+for arg in range(1, len(sys.argv)):
+    thd.append(sys.argv[arg])
+    save_to_json_file(thd, file)
