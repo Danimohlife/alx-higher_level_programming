910d50083f3d900404dfbec78fe53b10d3c10ba4
ddanimoh <ddanimoh@gmail.com>
2025-03-30 03:22:06 +0100

    str-rep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,10 @@ class Rectangle(Base):
         """
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """
+        string representation of Rectangle
+        """
+        return f"[Rectangle] ({self.id} {self.__x}/{self.__y} - \
+            {self.__width}/{self.__height})"
